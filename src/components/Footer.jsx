cdc387d60781ef513e742237ed3930d39ca1a1c0
msaraujo10 <msilva.araujo178@gmail.com>
2025-10-14 00:00:18 -0300

    Correção do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,11 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-6 text-center">
       <p>&copy; 2025 BB Vending Machine. Todos os direitos reservados.</p>
+      {/*
       <p className="mt-2 text-sm">
         Maior revendedora de Vending Machines do sul do Brasil.
       </p>
+      */}
     </footer>
   );
 };
