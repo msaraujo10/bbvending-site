13244917c1ee0c43390b64e18dd9dd6da4905ccd
msaraujo10 <msilva.araujo178@gmail.com>
2025-10-14 21:45:22 -0300

    Ajuste na Home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import HeroSection from "../components/HeroSection.jsx";
 import BenefitsSection from "../components/BenefitsSection.jsx";
 import MachinesCarousel from "../components/MachinesCarousel.jsx";
-import AboutSection from "../components/AboutSection.jsx";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
-      <AboutSection />
       <BenefitsSection />
       <MachinesCarousel />
     </>
